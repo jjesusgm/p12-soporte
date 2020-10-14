86663b057e4b165324c4e89dab5007347396787a
Jose de Jesus Gutierrez Martinez <jose.gutierrez2328@academicos.udg.mx>
2020-10-13 19:56:00 -0500

    Subida inicial de archivos
    Primer subida de archivos con las versiones iniciales

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Inicio | Soporte Prepa 12</title>
+	<link href="css/estilos.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+<header>
+<div class="logotipo">
+	<img src="imagenes/logo.jpg" alt="">
+</div>
+<p class="sitename">Escuela Preparatoria 12 - Soporte</p>
+<nav>
+	<ul>
+    	<li><a href="#">Inicio</a></li>
+    	<li><a href="#">Acerca de la Preparatoria</a></li>
+    	<li><a href="#">Oferta académica</a></li>
+    	<li><a href="#">Sistema Nacional de Bachillerato</a></li>
+    	<li><a href="#">Servicios</a></li>
+    	<li><a href="#">Sala de prensa</a></li>
+    	<li><a href="#">Comunidad</a></li>
+    </ul>
+</nav>
+<p class="path">Inicio</p>
+</header>
+
+<section class="main">
+	<section class="articles">
+    	<article>
+        	<h2>Este es el título de mi sitio web</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lorem urna, imperdiet nec fermentum nec, pulvinar non nulla. Nam mi est, efficitur vel scelerisque at, dignissim nec ante. Integer eu iaculis quam, rhoncus pellentesque risus. Fusce in quam ac velit interdum tempus. In volutpat velit felis, sed congue tortor vestibulum eu. Vestibulum iaculis fringilla nibh at faucibus. Sed iaculis ornare nibh ut maximus. Mauris ut pretium augue. Nunc placerat libero ultrices sapien aliquam lobortis. Aliquam erat volutpat. Phasellus enim elit, suscipit nec sodales ac, dapibus a urna. In nunc lacus, vulputate quis sem sed, egestas tempor metus. Aenean quis ligula sapien. Suspendisse pellentesque sodales turpis, in tincidunt massa ultrices id.</p>
+        </article>
+    	<article>
+        	<h2>Este es el título de mi sitio web</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lorem urna, imperdiet nec fermentum nec, pulvinar non nulla. Nam mi est, efficitur vel scelerisque at, dignissim nec ante. Integer eu iaculis quam, rhoncus pellentesque risus. Fusce in quam ac velit interdum tempus. In volutpat velit felis, sed congue tortor vestibulum eu. Vestibulum iaculis fringilla nibh at faucibus. Sed iaculis ornare nibh ut maximus. Mauris ut pretium augue. Nunc placerat libero ultrices sapien aliquam lobortis. Aliquam erat volutpat. Phasellus enim elit, suscipit nec sodales ac, dapibus a urna. In nunc lacus, vulputate quis sem sed, egestas tempor metus. Aenean quis ligula sapien. Suspendisse pellentesque sodales turpis, in tincidunt massa ultrices id.</p>
+        </article>
+    </section>
+    
+</section>
+
+<footer>
+	<p class="copyright">Jose de Jesus - Prepa12.sems.udg.mx</p>
+</footer>
+
+</body>
+</html>
---
+++ prueba.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Título de nuestro sitio</title>
+	<link href="css/estilos.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div>
+	<h2>#1</h2>
+</div>
+<div class="caja_padre">
+    <div class="caja">
+        <h2>#2</h2>
+    </div>
+</div>
+<div>
+	<h2>#3</h2>
+</div>
+<div>
+	<h2>#4</h2>
+</div>
+<div>
+	<h2>#5</h2>
+</div>
+<div>
+	<h2>#6</h2>
+</div>
+<div>
+	<h2>#7</h2>
+</div>
+<div>
+	<h2>#8</h2>
+</div>
+<div>
+	<h2>#9</h2>
+</div>
+<div>
+	<h2>#10</h2>
+</div>
+</body>
+</html>
