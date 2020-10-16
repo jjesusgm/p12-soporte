d2a37a27e4fbb36ddbc478615a5c67e75713f07c
Jose de Jesus Gutierrez Martinez <jose.gutierrez2328@academicos.udg.mx>
2020-10-16 16:22:42 -0500

    Opcacidad de imágenes y más
    - Se agragaron estilos para opacidad de imagenes al pasar el archivo por encima.
    - Estilos para enlace en el nombre del sitio.
    - Se le agregaron enlaces a las imagenes del encabezado.

--- index.php
+++ index.php
@@ -1,48 +1,68 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es-MX">
+
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Inicio | Soporte Prepa 12</title>
 	<link href="css/estilos.css" rel="stylesheet" type="text/css" />
 </head>
+
 <body>
 
-<header>
-<div class="logotipo">
-	<img src="imagenes/logo.jpg" alt="">
-</div>
-<p class="sitename">Escuela Preparatoria 12 - Soporte</p>
-<nav>
-	<ul>
-    	<li><a href="#">Inicio</a></li>
-    	<li><a href="#">Acerca de la Preparatoria</a></li>
-    	<li><a href="#">Oferta académica</a></li>
-    	<li><a href="#">Sistema Nacional de Bachillerato</a></li>
-    	<li><a href="#">Servicios</a></li>
-    	<li><a href="#">Sala de prensa</a></li>
-    	<li><a href="#">Comunidad</a></li>
-    </ul>
-</nav>
-<p class="path">Inicio</p>
-</header>
+	<header>
 
-<section class="main">
-	<section class="articles">
-    	<article>
-        	<h2>Este es el título de mi sitio web</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lorem urna, imperdiet nec fermentum nec, pulvinar non nulla. Nam mi est, efficitur vel scelerisque at, dignissim nec ante. Integer eu iaculis quam, rhoncus pellentesque risus. Fusce in quam ac velit interdum tempus. In volutpat velit felis, sed congue tortor vestibulum eu. Vestibulum iaculis fringilla nibh at faucibus. Sed iaculis ornare nibh ut maximus. Mauris ut pretium augue. Nunc placerat libero ultrices sapien aliquam lobortis. Aliquam erat volutpat. Phasellus enim elit, suscipit nec sodales ac, dapibus a urna. In nunc lacus, vulputate quis sem sed, egestas tempor metus. Aenean quis ligula sapien. Suspendisse pellentesque sodales turpis, in tincidunt massa ultrices id.</p>
-        </article>
-    	<article>
-        	<h2>Este es el título de mi sitio web</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lorem urna, imperdiet nec fermentum nec, pulvinar non nulla. Nam mi est, efficitur vel scelerisque at, dignissim nec ante. Integer eu iaculis quam, rhoncus pellentesque risus. Fusce in quam ac velit interdum tempus. In volutpat velit felis, sed congue tortor vestibulum eu. Vestibulum iaculis fringilla nibh at faucibus. Sed iaculis ornare nibh ut maximus. Mauris ut pretium augue. Nunc placerat libero ultrices sapien aliquam lobortis. Aliquam erat volutpat. Phasellus enim elit, suscipit nec sodales ac, dapibus a urna. In nunc lacus, vulputate quis sem sed, egestas tempor metus. Aenean quis ligula sapien. Suspendisse pellentesque sodales turpis, in tincidunt massa ultrices id.</p>
-        </article>
-    </section>
-    
-</section>
+		<div>
+			<a href="http://www.udg.mx/"><img src="imagenes/logo.jpg" class="logoredunivjal opaca"
+					alt="Red Universitaria de Jalisco"></a>
+			<a href="http://prepa12.sems.udg.mx/"><img src="imagenes/somos_prepa_12.png" class="logosomosp12 opaca"
+					alt="Somos Prepa 12"></a>
+		</div>
+		<p class="sitename"><a href="http://www.prepa12.sems.udg.mx/">Escuela Preparatoria 12 - Soporte</a></p>
+		<nav>
+			<ul>
+				<li><a href="#">Inicio</a></li>
+				<li><a href="#">1er ingreso</a></li>
+				<li><a href="#">Semestre Base</a></li>
+				<li><a href="#">Becas</a></li>
+				<li><a href="#">Contacto</a></li>
+				<li><a href="#">Ayuda</a></li>
+			</ul>
+		</nav>
+		<p class="path">Inicio</p>
+	</header>
 
-<footer>
-	<p class="copyright">Jose de Jesus - Prepa12.sems.udg.mx</p>
-</footer>
+	<section class="main">
+		<section class="articles">
+			<article>
+				<h2>Este es el título de mi sitio web</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lorem urna, imperdiet nec fermentum nec,
+					pulvinar non nulla. Nam mi est, efficitur vel scelerisque at, dignissim nec ante. Integer eu iaculis
+					quam, rhoncus pellentesque risus. Fusce in quam ac velit interdum tempus. In volutpat velit felis,
+					sed congue tortor vestibulum eu. Vestibulum iaculis fringilla nibh at faucibus. Sed iaculis ornare
+					nibh ut maximus. Mauris ut pretium augue. Nunc placerat libero ultrices sapien aliquam lobortis.
+					Aliquam erat volutpat. Phasellus enim elit, suscipit nec sodales ac, dapibus a urna. In nunc lacus,
+					vulputate quis sem sed, egestas tempor metus. Aenean quis ligula sapien. Suspendisse pellentesque
+					sodales turpis, in tincidunt massa ultrices id.</p>
+			</article>
+			<article>
+				<h2>Este es el título de mi sitio web</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lorem urna, imperdiet nec fermentum nec,
+					pulvinar non nulla. Nam mi est, efficitur vel scelerisque at, dignissim nec ante. Integer eu iaculis
+					quam, rhoncus pellentesque risus. Fusce in quam ac velit interdum tempus. In volutpat velit felis,
+					sed congue tortor vestibulum eu. Vestibulum iaculis fringilla nibh at faucibus. Sed iaculis ornare
+					nibh ut maximus. Mauris ut pretium augue. Nunc placerat libero ultrices sapien aliquam lobortis.
+					Aliquam erat volutpat. Phasellus enim elit, suscipit nec sodales ac, dapibus a urna. In nunc lacus,
+					vulputate quis sem sed, egestas tempor metus. Aenean quis ligula sapien. Suspendisse pellentesque
+					sodales turpis, in tincidunt massa ultrices id.</p>
+			</article>
+		</section>
+
+	</section>
+
+	<footer>
+		<p class="copyright">Jose de Jesus - Prepa12.sems.udg.mx</p>
+	</footer>
 
 </body>
+
 </html>
